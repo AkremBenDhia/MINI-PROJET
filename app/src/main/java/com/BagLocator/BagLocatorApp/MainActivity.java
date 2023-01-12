0d8ad27aac9342ce93f0d7c2db7b56f070511a3b
AkremBenDhia <121358963+AkremBenDhia@users.noreply.github.com>
2023-01-12 15:54:43 +0100

    Update MainActivity.java

--- app/src/main/java/com/BagLocator/BagLocatorApp/MainActivity.java
+++ app/src/main/java/com/BagLocator/BagLocatorApp/MainActivity.java
@@ -6,13 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity  {
 Button boutonStart;
 
