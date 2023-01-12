67f9bc619b97c7692c491dc3d95457003a6c9e6b
AkremBenDhia <121358963+AkremBenDhia@users.noreply.github.com>
2023-01-12 15:51:14 +0100

    Update MainActivity.java

--- app/src/main/java/com/BagLocator/BagLocatorApp/MainActivity.java
+++ app/src/main/java/com/BagLocator/BagLocatorApp/MainActivity.java
@@ -15,61 +15,27 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
 Button boutonStart;
-TextView inscrire;
-EditText mdp;
-SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db=openOrCreateDatabase("bagTrackDatabse",MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS USER (id integer primary key autoincrement, nom varchar(50), mdp varchar(50))");
 
-        inscrire=findViewById(R.id.inscrirewORD);
-        mdp=findViewById(R.id.mdp);
-
-        inscrire.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Inscription.class);
-                startActivity(intent);
-
-            }
-        });
         boutonStart= findViewById(R.id.bouton);
+
+
         boutonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String mdpValue = mdp.getText().toString();
-
-                if (mdpValue.isEmpty()) {
-                    mdp.setError("vide");
-                }
-                else{
-                    Cursor users = db.rawQuery("select * from USER where mdp=?", new String[]{mdpValue});
-                    if (users.getCount() == 0) {
-                        new AlertDialog.Builder(MainActivity.this).setTitle("Utilisateur inexistant").setMessage("Créer un compte ?").setPositiveButton("Oui", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent = new Intent(MainActivity.this, Inscription.class);
-                            startActivity(intent);
-                        }
-                        }).setNegativeButton("Non", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-
-                        }
-                        }).show();
-                    }
-                    if (users.getCount() > 0) {
-                    Intent intent = new Intent(MainActivity.this, Reglage_Client.class);
-                    intent.putExtra("mdp",mdpValue);
-                    startActivity(intent);
-                }
-            }
+             Intent intent = new Intent(MainActivity.this, Reglage_Client.class);
+             startActivity(intent);
             }
         });
+
+
+    }
+}
 
 
     }
